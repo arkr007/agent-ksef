d539177ecb37a2871d094e57d8c891e455a914d2
arkr007 <artur.kruk@optymalizator.pl>
2026-06-09 16:13:16 +0200

    Add Fastify API server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Agent KSeF startuje");
+import Fastify from "fastify";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.get("/", async () => {
+  return {
+    status: "ok",
+    name: "agent-ksef",
+  };
+});
+
+app.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+
+  console.log(`Server działa: ${address}`);
+});
